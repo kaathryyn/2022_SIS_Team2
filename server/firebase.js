e9421f9914bcae02dddf2561c92cff771d69103b
kaathryyn <48712975+kaathryyn@users.noreply.github.com>
2022-09-05 12:40:30 +1000

    Initialisation of Firebase application and functionalities

---
+++ server/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC7pD_Wbd6cqiQpxAFCNnnsHM3l3bZmGGo",
+  authDomain: "spr22-sis-team2.firebaseapp.com",
+  projectId: "spr22-sis-team2",
+  storageBucket: "spr22-sis-team2.appspot.com",
+  messagingSenderId: "672994537716",
+  appId: "1:672994537716:web:4a7af45030ed42b1782ea1"
+};
+
+const app = initializeApp(FIREBASE_CONFIG);
