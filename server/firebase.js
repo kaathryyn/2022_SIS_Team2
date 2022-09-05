69f5d463ddc09813ad45ccb801210b7bc4fff9e2
kaathryyn <48712975+kaathryyn@users.noreply.github.com>
2022-09-05 16:53:34 +1000

    Did a bunch of things for Firebase, but need to refactor/improve

--- server/firebase.js
+++ server/firebase.js
@@ -1,4 +1,6 @@
-import { initializeApp } from "firebase/app";
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+
+const SERVICE_ACCOUNT = require('../firebase_key.json');
 
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyC7pD_Wbd6cqiQpxAFCNnnsHM3l3bZmGGo",
@@ -9,4 +11,8 @@ const FIREBASE_CONFIG = {
   appId: "1:672994537716:web:4a7af45030ed42b1782ea1"
 };
 
-const app = initializeApp(FIREBASE_CONFIG);
+initializeApp({
+  credential: applicationDefault(),
+  projectId: 'Spr22_SIS_Team2',
+  storageBucket: 'spr22-sis-team2.appspot.com'
+});
