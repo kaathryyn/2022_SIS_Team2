1cbaf71f56dc3d4d6c3e62ef1ad54764d6a93868
stephhills <stephanie.e.hills@student.uts.edu.au>
2022-09-09 13:31:34 +1000

    Added form elements to login page

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,7 +1,32 @@
+import "./Login.css";
 
 function Login(){
     return(
-        <p>Hello World</p>
+        <div className="login-div">
+            <div className="login-card">
+                <form className="login-form">
+                    <h1 className="form-header"> login</h1>
+                    <div className="username-div">
+                        <h3 className="username-label">username</h3>
+                        <input className="input-field">
+
+                        </input>
+                    </div>
+                    <div className="password-div">
+                        <h3 className="password-label">password</h3>
+                        <input className="input-field">
+                        
+                        </input>
+                        <h3 className="forgot-password">forgot password</h3>
+                    </div>
+                    <div className="button-div">
+                        <button className="login-button">login</button>
+                        <button className="register-button">register</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     )
 }
 
