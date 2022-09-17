1f97e85abbd0bbe7b29427df7e568caebfd8d8f3
Joel3218 <13642499@student.uts.edu.au>
2022-09-17 17:24:20 +1000

    signin and sign out button added
    there is a problem with the sign in

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 
 import logo from './logo.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 import './App.css';
 
 function App() {
+  const [ user, setUser ] = useState({});
 
   function handleCallbackResponse(response){
     console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
   }
 
   useEffect(() => {
@@ -21,12 +32,25 @@ function App() {
       { theme: "outline", size: "large" }
 
     );
+
+    google.accounts.id.prompt();
   }, []);
-  
+  // if we have no user: show sign in button
+  // if we have a user: show log out button
   
   return (
     <div className="App">
       <div id="signInDiv"></div>
+      {Object.keys(user).length != 0 &&
+        <button onClick={ (e)  => handleSignOut(e)}>Sign Out</button>
+      }
+      
+      {user &&
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div> 
+      }
     </div>
   );
 }
