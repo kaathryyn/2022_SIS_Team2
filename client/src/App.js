174371ce3bd750ba4ae8a86cf01a733cb31471cd
Joel3218 <13642499@student.uts.edu.au>
2022-09-17 16:47:44 +1000

    initial commit to test login

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "http://142949275743-6tsrqr5ktajvn1rel7dm06kktlcc1gs0.apps.googleusercontent.com/",
+      callback: handleCallbackResponse
+  });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+
+    );
+  }, []);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
