8454bf7c18d678ab67aa191eb042ea32cea8bcd8
shafiqihtsham <Ihtsham123@outlook.com>
2022-10-07 17:59:41 +1100

    Moved camera to main frontend branch

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,14 @@
 import "./Login.css";
-
+import { Link } from "react-router-dom";
+import Navbar from "../Navbar/Navbar.js"
 import logo from "../../Assets/Main-Logo.png";
+import React from 'react';
 
-function Login(props){
-    console.log(props.history)
+function Login(){
     return(
+        <React.Fragment>   
+        <Navbar>
+       </Navbar>
         <div className="login-div">
                 <img
                     alt="Main Logo"
@@ -16,25 +20,25 @@ function Login(props){
                     <h1 className="form-header"> Login</h1>
                     <div className="username-div">
                         <h3 className="username-label">Username</h3>
-                        <input className="input-field">
-
-                        </input>
+                        <input className="input-field"></input>
                     </div>
                     <div className="password-div">
                         <h3 className="password-label">Password</h3>
-                        <input className="input-field">
-                        
-                        </input>
+                        <input className="input-field"></input>
                         <h3 className="forgot-password">Forgot Password?</h3>
                     </div>
+                        <Link to="/forgotpassword"
+                        className="forgot-password"> Forgot Password?</Link>
                     <div className="button-div">
                         <button className="login-button">Login</button>
-                        <button className="register-button" onClick={() => props.history.push("/signup")}>Register</button>
+                        <Link to="/Signup">
+                            <button className="register-button">Register</button>
+                        </Link>    
                     </div>
                 </form>
-
             </div>
         </div>
+        </React.Fragment>
     )
 }
 
