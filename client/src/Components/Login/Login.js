9c6aac4e577e7e424ac4a3a1d4bf22f3f2c16077
stephhills <stephanie.e.hills@student.uts.edu.au>
2022-10-07 11:22:35 +1100

    Adding elements to maintain consistency

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,9 +1,9 @@
 import "./Login.css";
+import { Link } from "react-router-dom";
 
 import logo from "../../Assets/Main-Logo.png";
 
-function Login(props){
-    console.log(props.history)
+function Login(){
     return(
         <div className="login-div">
                 <img
@@ -16,23 +16,20 @@ function Login(props){
                     <h1 className="form-header"> Login</h1>
                     <div className="username-div">
                         <h3 className="username-label">Username</h3>
-                        <input className="input-field">
-
-                        </input>
+                        <input className="input-field"></input>
                     </div>
                     <div className="password-div">
                         <h3 className="password-label">Password</h3>
-                        <input className="input-field">
-                        
-                        </input>
+                        <input className="input-field"></input>
                         <h3 className="forgot-password">Forgot Password?</h3>
                     </div>
                     <div className="button-div">
                         <button className="login-button">Login</button>
-                        <button className="register-button" onClick={() => props.history.push("/signup")}>Register</button>
+                        <Link to="/Signup">
+                            <button className="register-button">Register</button>
+                        </Link>    
                     </div>
                 </form>
-
             </div>
         </div>
     )
