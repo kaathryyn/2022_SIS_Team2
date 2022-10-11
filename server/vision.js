a59aae6f7f4d120dabe12e9336e1e65a2a4f8a1b
kaathryyn <48712975+kaathryyn@users.noreply.github.com>
2022-10-11 19:11:47 +1100

    Refactoring vision boundingBox implementation

--- server/vision.js
+++ server/vision.js
@@ -3,9 +3,7 @@ const vision = require("@google-cloud/vision");
 const { ok } = require("assert");
 const GCP_CREDENTIALS = require("./GCP_CREDS.json");
 
-const CREDENTIALS = JSON.parse(
-    JSON.stringify(GCP_CREDENTIALS)
-);
+const CREDENTIALS = JSON.parse(JSON.stringify(GCP_CREDENTIALS));
 
 const CONFIG = {
     credentials: {
@@ -31,33 +29,21 @@ async function detectLandmark(img) {
     // landmarkAnnotations[0] = result with highest score (match)
     if (landmarks[0]?.description) {
         console.log(`Landmark identified for file ${img}:`);
-        landmarks.forEach(l => console.log(l.description));
+        landmarks.forEach((l) => console.log(l.description));
         landmarkVertices = landmarks[0].boundingPoly.vertices;
-        
-        // Getting Coords of Bounding Box
-        var topLeft_x = landmarkVertices[0].x;
-        var topLeft_y = landmarkVertices[0].y;
-        var topRight_x = landmarkVertices[1].x;
-        var topRight_y = landmarkVertices[1].y;
-        var bottomRight_x = landmarkVertices[2].x;
-        var bottomRight_y = landmarkVertices[2].y;
-        var bottomLeft_x = landmarkVertices[3].x;
-        var bottomLeft_y = landmarkVertices[3].y;
 
-        // Pushing all landmark information to variable
+        //Adding landmark name to landmarkInfo[0]
         landmarkInfo.push(landmarks[0].description);
-        // Bounding box order = top_left > top_right > bottom_right > bottom_left
-        landmarkInfo.push(topLeft_x);
-        landmarkInfo.push(topLeft_y);
-        landmarkInfo.push(topRight_x);
-        landmarkInfo.push(topRight_y);
-        landmarkInfo.push(bottomRight_x);
-        landmarkInfo.push(bottomRight_y);
-        landmarkInfo.push(bottomLeft_x);
-        landmarkInfo.push(bottomLeft_y);
+
+        // Add Coords of Bounding Box to array - top_left > top_right > bottom_right > bottom_left
+        for (let i = 0; i < 4; i++) {
+            landmarkInfo.push(landmarkVertices[i].x);
+            landmarkInfo.push(landmarkVertices[i].y);
+        }
+    } else {
+        console.log(`No famous landmark detected for file ${img}`);
+        console.log("----------------------------------------------------------------------------------------------------");
     }
-    else console.log(`No famous landmark detected for file ${img}`);
-    console.log("----------------------------------------------------------------------------------------------------");
 
     console.log(landmarkInfo);
     return landmarkInfo;
@@ -77,8 +63,6 @@ const path = require("path");
 const files = fs.readdirSync(path.resolve(process.env.TEST_LOC));
 // files.forEach((path) => detectLandmark("./test_landmarks/" + path));
 // console.log("====================================================================================================");
-
-
 
 
 // Exposure points
