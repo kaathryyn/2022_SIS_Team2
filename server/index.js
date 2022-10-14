e7432c3827ac61e9a035627905ffaac963f3c127
willhtang <62323516+willhtang@users.noreply.github.com>
2022-10-14 17:39:44 +1100

    Backend (#11)
    * Installed cloud vision api
    
    * Basic Vision API functionality implemented
    
    * Testing with different images + added message for when no landmark is detected
    
    * Vision API batch testing
    
    Result: Detects famous landmarks photographed at certain angles.
    
    Comments: Probably primarily detects landmarks by shape patterns.
    
    * Separate API file
    
    * Initial commit
    
    Setting up the environment
    
    * Initialisation of Firebase application and functionalities
    
    * Did a bunch of things for Firebase, but need to refactor/improve
    
    * Refactoring firebase implementation
    
    * More refactoring and trying to fix Vision API connection to Firebase storage
    
    * Fixed Vision API
    
    Line 34: Used wrong API reference.
    
    * Express API tests
    
    * File upload - Part 1
    
    Can test using Postman:
    - Post request to http://localhost:3001/upload
    - Set Body field to form-data
    - Set key-value pair to image:[file]
    - Insert an image file and post
    
    * File upload - Part 2 => Vision API Endpoint
    
    Test as per instructions in Part 1.
    
    * initial commit to test login
    
    * some changes to fix errors
    
    * signin and sign out button added
    
    there is a problem with the sign in
    
    * Fixed client_id for Google login
    
    * Removing GCP credentials from repo for security
    
    * Quick fix to gcp credentials for vision api
    
    * Quick package-lock.json fix
    
    * Retrieving boundingBox values from vision API results
    
    * Refactoring vision boundingBox implementation
    
    * Implemented change to return coords in an array
    
    * Correction to firebase initialisation
    
    * Reading data from firestore
    
    * Blank endpoints
    
    * Basic Login
    
    + AddUser
    + CheckUser
    
    * Basic gallery endpoint
    
    *untested*
    
    * Updates to endpoints
    
    * Upload image functionality
    
    * Secured endpoints
    
    Co-authored-by: kaathryyn <48712975+kaathryyn@users.noreply.github.com>
    Co-authored-by: willhtang <99226482+will-verser@users.noreply.github.com>
    Co-authored-by: Joel3218 <13642499@student.uts.edu.au>

--- server/index.js
+++ server/index.js
@@ -1,9 +1,124 @@
-const express = require("express");
+
+// const express = require("express");
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+require('dotenv').config();
+
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const socketio = require('socket.io');
+const vision = require("./vision");
+const fb = require("./firebase");
+
+
+//Initialise server
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+    console.log('New WS Connection...')
+});
 
 const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const app = express();
+// Incoming file storage function
+const multer = require("multer");
+const { ppid } = require('process');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, process.env.UPLOAD_LOC);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+var upload = multer({ storage: storage });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+
+// API Endpoints
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); // Change this for different HTTP bodies
+app.get('/', (req, res)=>{
+    console.log(req.headers);
+    res.send("Welcome to your server");
+});
+
+// Retrieves all user photos
+app.get('/gallery', async(req, res) => {
+    const docId = req.body;
+    if (docId.length === 20) {
+        const result = await fb.getGallery(docId);
+        return res.send(result);
+    }
+    res.status(400).send("Bad Request!");
+});
+
+// Retrieves location data
+app.get('/landmark', async(req, res) => {
+
+});
+
+// Runs Vision AI on incoming image
+app.post('/vision', upload.single("image"), async(req, res)=>{
+    try {
+        const file = req.file.filename;
+        const loc = req.file.destination;
+        const path = "./" + loc + "/" + file;
+        // const result = await vision.detectLandmark(path); // Commented to reduce usage
+        // res.send(result);
+        res.send("Done");
+    }
+    catch { res.status(405).send("Failed to process request."); }
+});
+
+// Add to User Gallery
+app.post('/upload', upload.single("image"), async(req, res)=>{
+    const {id, landmark} = req.body;
+    if (!(id && landmark)) { return res.status(400).send("Bad Request!"); }
+    try {
+        const filename = req.file.filename;
+        const location = req.file.destination;
+        const path = "./" + location + "/" + filename;
+    
+        const fs = require("fs");
+        const blob = fs.readFileSync(path, null);
+        const result = fb.addToGallery(id, blob, landmark) // Boolean returned
+        res.send(result);
+    }
+    catch { res.status(405).send("Failed to process request."); }
+});
+
+// Checks if user can log in
+app.post('/login', async(req, res) => {
+    const login = req.body;
+    if (typeof user === "object") {
+        const result = await fb.checkUser(login); // User ID is returned
+        return result && res.send(result);
+    }
+    res.status(400).send("Bad Request!");
+    
+});
+
+// Create new user
+app.post('/signup', async(req, res) => {
+    const user = req.body;
+    if (typeof user === "object") {
+        const result = await fb.addUser(user); // User ID is returned
+        return res.send(result);
+    }
+    res.status(400).send("Bad Request!");
 });
---
+++ server/vision.js
@@ -0,0 +1,68 @@
+// VISION API
+const vision = require("@google-cloud/vision");
+const { ok } = require("assert");
+const GCP_CREDENTIALS = require("./GCP_CREDS.json");
+
+const CREDENTIALS = JSON.parse(JSON.stringify(GCP_CREDENTIALS));
+
+const CONFIG = {
+    credentials: {
+        private_key: CREDENTIALS.private_key,
+        client_email: CREDENTIALS.client_email,
+    },
+};
+
+const bucketName = "gs://spr22-sis-team2.appspot.com/Test";
+const filename = "sydney-opera-house-selfie.jpg";
+// var img = `${bucketName}/${filename}`;
+var img = "gs://cloud-samples-data/vision/landmark/st_basils.jpeg";
+
+async function detectLandmark(img) {
+    const client = new vision.ImageAnnotatorClient(CONFIG);
+
+    var landmarkInfo = [];
+    var landmarkVertices;
+
+    const [result] = await client.landmarkDetection(img);
+    const landmarks = result.landmarkAnnotations;
+
+    // landmarkAnnotations[0] = result with highest score (match)
+    if (landmarks[0]?.description) {
+        console.log(`Landmark identified for file ${img}:`);
+        landmarks.forEach((l) => console.log(l.description));
+        landmarkVertices = landmarks[0].boundingPoly.vertices;
+
+        //Adding landmark name to landmarkInfo[0]
+        landmarkInfo.push(landmarks[0].description);
+
+        // Add Coords of Bounding Box to array - top_left > top_right > bottom_right > bottom_left
+        for (let i = 0; i < 4; i++) {
+            landmarkInfo.push(Object.values(landmarkVertices[i]));
+        }
+    } else {
+        console.log(`No famous landmark detected for file ${img}`);
+        console.log("----------------------------------------------------------------------------------------------------");
+    }
+
+    console.log(landmarkInfo);
+    return landmarkInfo;
+}
+
+// detectLandmark(img);
+
+////////////////////////////
+///////--- Tests ---////////
+////////////////////////////
+console.log("====================================================================================================");
+console.log("Beginning Tests...");
+console.log("Batch Read");
+console.log("====================================================================================================");
+const fs = require("fs");
+const path = require("path");
+const files = fs.readdirSync(path.resolve(process.env.TEST_LOC));
+// files.forEach((path) => detectLandmark("./test_landmarks/" + path));
+// console.log("====================================================================================================");
+
+
+// Exposure points
+module.exports = { detectLandmark };
