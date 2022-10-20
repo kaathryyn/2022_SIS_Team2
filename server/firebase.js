33dabd4978f1a4c457abc1541262c82d8dbeae34
willhtang <will.h.tang@gmail.com>
2022-10-21 00:11:36 +1100

    Manual backend merge

--- server/firebase.js
+++ server/firebase.js
@@ -1,4 +1,6 @@
-const { initializeApp, cert } = require('firebase-admin/app');
+const admin = require('firebase-admin');
+const { getDatabase, getDatabaseWithUrl } = require('firebase-admin/database');
+const { user } = require('firebase-functions/v1/auth');
 
 const SERVICE_ACCOUNT = require('../firebase_key.json');
 
@@ -12,13 +14,91 @@ const FIREBASE_CONFIG = {
 };
 
 try {
-  initializeApp({
-    credential: cert(SERVICE_ACCOUNT),
-    projectId: FIREBASE_CONFIG.projectId,
-    storageBucket: FIREBASE_CONFIG.storageBucket
+  admin.initializeApp({
+    credential: admin.credential.cert(SERVICE_ACCOUNT),
+    ...FIREBASE_CONFIG
   });
   console.log("Firebase app is now running...")
 }
 catch(err) {
   console.log("Error! Firebase app cannot start :(")
 }
+
+const firestore = admin.firestore();
+
+function checkId(id) {
+  const regEx = /^[a-z0-9]+$/i;
+  return regEx.exec(id);
+}
+
+function encodeData(data) {
+  return Buffer.from(data).toString("utf16le");
+}
+
+async function getAllUsers() {
+  await firestore.collection("users").get().then(query => {
+    query.forEach(document => {
+      console.log(JSON.stringify(document.data()));
+    });
+  });
+}
+
+async function getUser(username) {
+  let user = null;
+  let id = null;
+  await firestore.collection("users").get().then(query => {
+    query.forEach(document => {
+      if (document.data().Email === username) { user = document.data(); id = document.id; return; };
+    });
+  });
+  return {user, id};
+}
+
+async function addUser(user) {
+  const { email, name, password } = user;
+  if (!(email && name && password)) return null;
+  if (getUser(email)) return null;
+
+  user.password = encodeData(password);
+  const res = await firestore.collection("users").add(user);
+  console.log("Added new document with ID: ", res?.id);
+  return res?.id;
+}
+
+async function checkUser(credentials) {
+  let returnFlag = false;
+  const { email, password } = credentials;
+  if (!(email && password)) return null;
+
+  const {user, id} = await getUser(credentials.Email);
+  if (credentials.Password === encodeData(user.Password)) { returnFlag = true };
+  return returnFlag && id;
+}
+
+async function addToGallery(id, image, landmark) {
+  try {
+    const imageDoc = {
+      imageContent: image,
+      landmark: landmark,
+      dateCreated: Date.now(),
+    }
+    const res = await firestore.collection("users").doc(id).collection("gallery").add(imageDoc);
+    console.log("Added new image with ID: ", res.id);
+    return true;
+  }
+  catch { return false; }
+}
+
+async function getGallery(id) {
+  let gallery = [];
+  if (!checkId(id)) return null;
+  await firestore.collection("users").doc(id).collection("gallery").get().then(query => {
+    query.forEach(document => {
+      gallery.push(document.data());
+    })
+  });
+  return gallery;
+}
+
+// Exposure points
+module.exports = { checkUser, addUser, getGallery, addToGallery };
