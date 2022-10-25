1712a39d82b1c0de4f652dfe4179ac83999401bc
kaathryyn <48712975+kaathryyn@users.noreply.github.com>
2022-10-25 21:12:38 +1100

    So close - managed to get urlForWikiPageByPageID working, but still need actual wiki URLs for the landmark

--- server/wikiAPI.js
+++ server/wikiAPI.js
@@ -1,12 +1,11 @@
+const { LocalConvenienceStoreOutlined } = require('@material-ui/icons');
 const fetch = require('node-fetch');
 
 async function getLandmarkWikiURL() {
   var wikiItems = [];
   var wikiItemsPageID = [];
   var wikiItemsPageTitle = [];
-  var urlForWikiPageByPageID;
-  var urlForLandmarkPage;
-  var page;
+  var urlForWikiPageByPageID; // This is a .json file containing the actual URL for each wiki page
   var pageID;
 
   var landmark = 'sydney_opera_house';
@@ -16,50 +15,50 @@ async function getLandmarkWikiURL() {
   await fetch(url)
     .then((res) => {
       res = res.json();
-      // console.log(`First response: ${res}`);
       return res;
     })
     .then((res) => {
-      console.log(`Second response: ${res}`);
       for (var key in res.query.pages) {
-        //This is not pushing items correctly
-        // wikiItems.push({
-        //   queryResultPageID: res.query.pages[key].pageid,
-        //   queryResultPageTitle: res.query.pages[key].title
-        // });
-        wikiItemsPageID.push(key);
+        //Pushing values to separate arrays
+        wikiItemsPageID.push(res.query.pages[key].pageid);
         wikiItemsPageTitle.push(res.query.pages[key].title);
 
-        //Trying to make an object with pageIDs and page Titles
-        wikiItems = Object.assign(wikiItemsPageID, wikiItemsPageTitle);
-        console.log(`${res.query.pages[key].pageid}: ${res.query.pages[key].title}`);
-        // console.log(`WikiItemPageIDs: ${wikiItemsPageID[key]}`);
-        // console.log(`WikiItemsPageTitle: ${wikiItemsPageTitle[key]}`);
+        //Combines pageids and page_titles into one "object"
+        for (let i = 0; i < wikiItemsPageID.length; i++) {
+          wikiItems[wikiItemsPageID[i]] = String(wikiItemsPageTitle[i])
+        }
       }
-      console.log(`WikiItems: ${Object.entries(wikiItems)}`);
+      // for (let i = 0; i < wikiItemsPageID.length; i++) {
+      //   console.log(`PageID: ${wikiItemsPageID[i]}`);
+      // }
+      // console.log(`WikiItems: ${Object.entries(wikiItems)}`);
+      return res
     }
     )
     .then((res) => {
-      //wikiItems = counter of pages associated with identified landmark in Wiki
-      for (var item in wikiItems) {
-        page = wikiItems[item];
-        pageID = page.queryResultPageID;
+      for (var item in wikiItemsPageID) {
+        pageID = wikiItemsPageID[item];
         urlForWikiPageByPageID = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageID}&inprop=url&format=json`;
-      }
-      console.log(`Third Response: ${res}`);
+        console.log(pageID);
+        console.log(`URL for WikiPage: ${urlForWikiPageByPageID}`);
+        
+        fetch(urlForWikiPageByPageID)
+          .then((res) => {
+            res = res.json();
+            return res
+          })
+          .then((res) => {
+            // console.log(`res.query.pages[pageID]: ${Object.entries(res.query.pages)}`)
+            console.log(`Item: ${pageID}`)
+            // finalPageID = res.query.pages[pageID].pageid;
+            // console.log(`finalPageID: ${finalPageID}`);
+            // urlForLandmarkPage = res.query.pages[pageID].fullurl;
+          }
+          )
+        }
     })
-  fetch(urlForWikiPageByPageID)
-    .then((res) => {
-      return res.json()
-    })
-    .then((res) => {
-      console.log(`Page ID = ${pageID}`);
-      urlForLandmarkPage = res.query.pages[pageID].fullurl;
-    }
-    )
-    .then(console.log(`Landmark Wiki URL: ${urlForLandmarkPage}`))
-}
 
+}
 
 var result = getLandmarkWikiURL();
 // console.log(`Landmark Wiki URL: ${result}`);
