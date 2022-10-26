4911b31b40e0186ef89488755e31e0c1fbb933a1
kaathryyn <48712975+kaathryyn@users.noreply.github.com>
2022-10-26 21:01:53 +1100

    Initial connection of signup to backend
    Scuffed commit, since my vscode isn't syncing properly with the repo for some reason.... :/

--- client/src/Components/SignUp/Signup.js
+++ client/src/Components/SignUp/Signup.js
@@ -3,43 +3,70 @@ import logo from "../../Assets/Main-Logo.png";
 import { Link } from "react-router-dom";
 import './signup.css'
 function SignUp() {
+    let history = useNavigate();
+    const [email, setEmail] = useState("");
+    const [fullName, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const onFinish = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:3001/signup", {
+                ...{
+                    email: email,
+                    name: fullName,
+                    password: password,
+                },
+            })
+            .then((res) => {
+                localStorage.setItem("user", res.data);
+                history("/login");
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("Unable to sign up");
+            });
+    };
     
     return(
       <div className="register-div">
-         <a href="/home">  
-        <img
-                    alt="Main Logo"
-                    src={logo}
-                    className="main-logo"
-                ></img>
-                </a>
+        <a href="/home">  
+          <img alt="Main Logo" src={logo} className="main-logo"></img>
+        </a>
         <div className="register-card">
             <form className="register-form">
-                <h1 className="register-header">Signup</h1>
-                <div className="first-name-div">
-                    <h3 className="first-name-label">First Name</h3>
-                    <input className="register-input-field"></input>
-                </div>
-                <div className="last-name-div">
-                    <h3 className="last-name-label">Last Name</h3>
-                    <input className="register-input-field"></input>
-                </div>
-                <div className="register-email-div">
-                    <h3 className="register-email-label">Email</h3>
-                    <input className="register-input-field"></input>
-                </div>
-                <div className="register-password-div">
-                    <h3 className="register-password-label">Password</h3>
-                    <input className="register-input-field"></input>
-                </div>
-                <div className="confirm-password-div">
-                    <h3 className="confirm-password-label">Confirm Password</h3>
-                    <input className="register-input-field"></input>
-                </div>
-                <Link to="/login">
-                    <button className="signup-button">Sign Up</button>
-                </Link>
-                
+                <h1 className="register-header">Sign Up</h1>
+                    <TextField
+                        label="Name"
+                        variant="standard"
+                        required
+                        value={fullName}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <TextField
+                        label="Email"
+                        variant="standard"
+                        required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        label="Password"
+                        variant="standard"
+                        type={"password"}
+                        required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <TextField
+                        label="Confirm Password"
+                        variant="standard"
+                        type={"password"}
+                        required
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                <button className="signup-button">Sign Up</button>
             </form>
         </div>
       </div>      
